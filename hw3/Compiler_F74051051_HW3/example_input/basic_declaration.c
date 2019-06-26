d2f7d46f1feb100303e528abb14ce59f3d6c5434
NewmiLeou <newmi.leou@gmail.com>
2019-06-26 15:15:12 +0800

    clear unused thing

--- hw3/Compiler_F74051051_HW3/example_input/basic_declaration.c
+++
@@ -1,9 +0,0 @@
-int a = 6;
-int b;
-void main() {
-    int d;
-    d = a + 6;
-    print(d);
-
-    return;
-}
--- hw3/Compiler_F74051051_HW3/example_input/basic_while_statement.c
+++
@@ -1,9 +0,0 @@
-void main(){
-    int a = 1;
-    while (a > 6){
-        print(a);
-        a++;
-    }
-    return;
-}
-
